85cc88e3470f16ef48e28d7df017daa32d48bd8a
shiba <shibadog0410@gmail.com>
2021-05-04 01:03:57 +0900

    fix hatimii.sh

--- hatimii.sh
+++ hatimii.sh
@@ -1,13 +1,58 @@
 #!/bin/bash
+
+INPUT=""
+USERINPUT=""
+HATIMII="はちみー"
+WONAMERUTO="をなめると"
+ASHIGAA="あしがー"
+HAYAKUUNARUU="はやくーなる"
+
 while :
 do
-    read -p "Repeat ? (y/n): " DATA
-    if [ "$DATA" = "y" ]; then
-        echo "Repeat !!"
-    elif [ "$DATA" = "n" ]; then
-        echo "End repead."
-        break
-    else
-        echo "Input y or n key"
-    fi
+    while :
+    do
+        read -p "0:${HATIMII}, 1:${WONAMERUTO} " DATA
+        if [ "$DATA" = "0" ]; then
+            INPUT="${INPUT}${HATIMII} "
+            USERINPUT="${USERINPUT}0"
+        elif [ "$DATA" = "1" ]; then
+            INPUT="${INPUT}${WONAMERUTO} "
+            USERINPUT="${USERINPUT}1"
+                read -p "0:${ASHIGAA}, 1:${HATIMII} " DATA
+                if [ "$DATA" = "0" ]; then
+                    INPUT="${INPUT}${ASHIGAA} "
+                    USERINPUT="${USERINPUT}0"
+                    break
+                elif [ "$DATA" = "1" ]; then
+                    INPUT="${INPUT}${HATIMII} "
+                    USERINPUT="${USERINPUT}1"
+                    continue
+                fi
+            break
+        fi
+    done
+
+    while :
+    do
+        read -p "0:${ASHIGAA}, 1:${HAYAKUUNARUU} " DATA
+        if [ "$DATA" = "0" ]; then
+            INPUT="${INPUT}${ASHIGAA} "
+            USERINPUT="${USERINPUT}0"
+        elif [ "$DATA" = "1" ]; then
+            INPUT="${INPUT}${HAYAKUUNARUU} "
+            USERINPUT="${USERINPUT}1"
+                read -p "0:はちみー, 1:終了 " DATA
+                if [ "$DATA" = "0" ]; then
+                    INPUT="${INPUT}${HATIMII} "
+                    USERINPUT="${USERINPUT}0"
+                    break
+                elif [ "$DATA" = "1" ]; then
+                    USERINPUT="${USERINPUT}1"
+                    echo "$INPUT"
+                    echo "$USERINPUT"
+                    exit
+                fi
+            break
+        fi
+    done
 done
